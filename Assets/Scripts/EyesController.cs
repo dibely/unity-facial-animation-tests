6367d3a891037ad55fe3a2449af0a787cd517fff
Duane Bradbury <dibely@hotmail.com>
2017-10-05 10:46:17 +0100

    Re-factored the eye controller to break down the main update method into discrete methods of functionality and improve readability.

--- Assets/Scripts/EyesController.cs
+++ Assets/Scripts/EyesController.cs
@@ -57,79 +57,87 @@ public class EyesController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (eyelidAnimators.Length > 0) {
-            timeToNextBlink -= Time.deltaTime;
-
-            if (timeToNextBlink <= 0.0f) {
-                for (int index = 0; index < eyelidAnimators.Length; index++) {
-                    eyelidAnimators[index].SetTrigger("blink");
-                }
-
-                timeToNextBlink = Random.Range(2.0f, 5.0f);
-            }
-
-            if (eyes.Length > 0) {
-                if (pointOfInterest != null) {
-                    for (int index = 0; index < eyes.Length; index++) {
-                        // Calculate the angle between the eye and target point and use that to offset the UV coords
-                        GameObject eye = eyes[index];
-                        Vector3 facing = eye.transform.forward;
-                        Vector3 directionToPointOfInterest = eye.transform.position - pointOfInterest.position;
-
-                        Vector3 directionXZ = new Vector3(directionToPointOfInterest.x, facing.y, directionToPointOfInterest.z);
-                        directionXZ.Normalize();
-
-                        Vector3 directionY = new Vector3(facing.x, directionToPointOfInterest.y, facing.z);
-                        directionY.Normalize();
-
-                        float productXZ = Vector3.Dot(directionXZ, facing);
-                        float productY = Vector3.Dot(directionY, eye.transform.up);
-
-                        //Debug.Log("Facing = " + facing + " Up = " + eye.transform.up);
-                        //Debug.Log("XZ = " + productXZ + "("+ directionXZ + ")");
-                        //Debug.Log("Y = " + productY + "(" + directionY + ")");
-
-                        // Result of the dot product is as follows:
-                        // 1 means vectors are parallel in the same direction
-                        // 0 means vectors are perpendicular (either side)
-                        // > 0 is vectors are opposing i.e. facing away from each other beyond 90 degrees either side, with -1 being directly opposite
-                        // We are only interested in the vector in XZ-axes where the resulting dot proiduct is > 0 i.e. in front of the object
-
-                        float targetOffsetX = 0.0f;
-                        float targetOffsetY = 0.0f;
-
-                        if (productXZ >= 0.0f) {
-                            // Do a dot product with the tangent vector of the facing direction to determine which side the direction vector is in relation to the facing direction.
-                            // This allows us to alter the sign of the final UV offset in the X axis to fit within our -0.5 to 0.5 range.
-                            float sign = (Vector3.Dot(directionXZ, eye.transform.right) > 0.0f ? -1.0f : 1.0f);
-                            targetOffsetX = ((1.0f - productXZ) * uvOffsetLimit) * sign;
-                            targetOffsetY = productY * uvOffsetLimit;
-                        }
-
-                        // Linearly interpolate towards the target offsets to avoid instant changes in the eye offsets
-                        float offsetX = eyeMaterials[index].GetFloat("_OffsetX");
-                        float offsetY = eyeMaterials[index].GetFloat("_OffsetY");
-
-                        offsetX = Mathf.Lerp(offsetX, targetOffsetX, eyeInterpolationRate);
-                        offsetY = Mathf.Lerp(offsetY, targetOffsetY, eyeInterpolationRate);
-
-                        eyeMaterials[index].SetFloat("_OffsetX", offsetX);
-                        eyeMaterials[index].SetFloat("_OffsetY", offsetY);
-                    }
-                }
-                else {
-                    for (int index = 0; index < eyeMaterials.Length; index++) {
-                        // Linearly interpolate towards the target offsets to avoid instant changes in the eye offsets
-                        float offsetX = eyeMaterials[index].GetFloat("_OffsetX");
-                        float offsetY = eyeMaterials[index].GetFloat("_OffsetY");
-
-                        offsetX = Mathf.Lerp(offsetX, 0.0f, eyeInterpolationRate);
-                        offsetY = Mathf.Lerp(offsetY, 0.0f, eyeInterpolationRate);
-
-                        eyeMaterials[index].SetFloat("_OffsetX", offsetX);
-                        eyeMaterials[index].SetFloat("_OffsetY", offsetY);
-                    }
-                }
-            }
+            UpdateEyelids();
         }
+
+        if (eyes.Length > 0) {
+            UpdateEyes();
+        }
+    }
+
+    private void UpdateEyelids() {
+        timeToNextBlink -= Time.deltaTime;
+
+        if (timeToNextBlink <= 0.0f) {
+            for (int index = 0; index < eyelidAnimators.Length; index++) {
+                eyelidAnimators[index].SetTrigger("blink");
+            }
+
+            timeToNextBlink = Random.Range(2.0f, 5.0f);
+        }
+    }
+
+
+    private void UpdateEyes() {
+        for (int index = 0; index < eyes.Length; index++) {
+            Vector2 targetOffset = new Vector2(0,0);
+            if(pointOfInterest != null) {
+                targetOffset = DetermineEyeOffsetForPointOfInterest(index);
+            }
+  
+            UpdateEyeToLookAtTarget(index, targetOffset);
+        }
+    }
+
+    private Vector2 DetermineEyeOffsetForPointOfInterest(int index) {
+        // Calculate the angle between the eye and target point and use that to offset the UV coords
+        GameObject eye = eyes[index];
+        Vector3 facing = eye.transform.forward;
+        Vector3 directionToPointOfInterest = eye.transform.position - pointOfInterest.position;
+
+        Vector3 directionXZ = new Vector3(directionToPointOfInterest.x, facing.y, directionToPointOfInterest.z);
+        directionXZ.Normalize();
+
+        Vector3 directionY = new Vector3(facing.x, directionToPointOfInterest.y, facing.z);
+        directionY.Normalize();
+
+        float productXZ = Vector3.Dot(directionXZ, facing);
+        float productY = Vector3.Dot(directionY, eye.transform.up);
+
+        //Debug.Log("Facing = " + facing + " Up = " + eye.transform.up);
+        //Debug.Log("XZ = " + productXZ + "("+ directionXZ + ")");
+        //Debug.Log("Y = " + productY + "(" + directionY + ")");
+
+        // Result of the dot product is as follows:
+        // 1 means vectors are parallel in the same direction
+        // 0 means vectors are perpendicular (either side)
+        // > 0 is vectors are opposing i.e. facing away from each other beyond 90 degrees either side, with -1 being directly opposite
+        // We are only interested in the vector in XZ-axes where the resulting dot proiduct is > 0 i.e. in front of the object
+
+        float targetOffsetX = 0.0f;
+        float targetOffsetY = 0.0f;
+
+        if (productXZ >= 0.0f) {
+            // Do a dot product with the tangent vector of the facing direction to determine which side the direction vector is in relation to the facing direction.
+            // This allows us to alter the sign of the final UV offset in the X axis to fit within our -0.5 to 0.5 range.
+            float sign = (Vector3.Dot(directionXZ, eye.transform.right) > 0.0f ? -1.0f : 1.0f);
+            targetOffsetX = ((1.0f - productXZ) * uvOffsetLimit) * sign;
+            targetOffsetY = productY * uvOffsetLimit;
+        }
+
+        return new Vector2(targetOffsetX, targetOffsetY);
+    }
+
+
+    private void UpdateEyeToLookAtTarget(int index, Vector2 targetOffset) {
+        // Linearly interpolate towards the target offsets to avoid instant changes in the eye offsets
+        float offsetX = eyeMaterials[index].GetFloat("_OffsetX");
+        float offsetY = eyeMaterials[index].GetFloat("_OffsetY");
+
+        offsetX = Mathf.Lerp(offsetX, targetOffset.x, eyeInterpolationRate);
+        offsetY = Mathf.Lerp(offsetY, targetOffset.y, eyeInterpolationRate);
+
+        eyeMaterials[index].SetFloat("_OffsetX", offsetX);
+        eyeMaterials[index].SetFloat("_OffsetY", offsetY);
     }
 }
