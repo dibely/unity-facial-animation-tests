5d9e45c6d4272c19aaa34e5c3f91e8c910898f88
Duane Bradbury <dibely@hotmail.com>
2017-10-16 11:29:05 +0100

    Added an octopus test model that faces positive-z to allow for correct setup of the eye direction. Added octopus unique textures, materials and shaders for tweaking without affecting the existing assets. NOTE: These were also required because there are multiple UV channels in the model that need to be catered for. Added a specific octopus eye controller script that works with the new model. Applied new octopus specific eyelid and mouth animations.

---
+++ Assets/Scripts/OctopusEyesController.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+
+public class OctopusEyesController : MonoBehaviour {
+    public GameObject[] eyelids;
+    public string eyeMaterialName = "eye";
+    public GameObject[] eyes;
+    public Transform pointOfInterest;
+    private Animator[] eyelidAnimators;
+    private Material[] eyeMaterials;
+
+    [Range(0.1f, 0.5f)]
+    public float uvOffsetLimit = 0.25f;
+
+    [Range(0.01f, 1.0f)]
+    public float eyeInterpolationRate = 0.1f;
+
+    private float timeToNextBlink = 5.0f;
+
+    // Use this for initialization
+    void Start() {
+        if (eyelids.Length > 0) {
+            eyelidAnimators = new Animator[eyelids.Length];
+            for (int index = 0; index < eyelids.Length; index++) {
+                GameObject eyelid = eyelids[index];
+                Animator eyelidAnimator = eyelid.GetComponent<Animator>();
+                if (eyelidAnimator != null) {
+                    eyelidAnimators[index] = eyelidAnimator;
+                }
+                else {
+                    Debug.LogError("Failed to find animator on object " + eyelid.name);
+                }
+            }
+        }
+
+        if (eyes.Length > 0) {
+            eyeMaterials = new Material[eyes.Length];
+
+            for (int index = 0; index < eyes.Length; index++) {
+                GameObject eye = eyes[index];
+                MeshRenderer meshRenderer = eye.GetComponent<MeshRenderer>();
+
+                if (meshRenderer != null) {
+                    Material eyeMaterial = null;
+
+                    if (eyeMaterialName == null || eyeMaterialName.Length == 0) {
+                        // Just use the first one we find if no name set
+                        eyeMaterial = meshRenderer.material;
+                    }
+                    else {
+                        // Search through the materials for the correct one
+
+                        for (int materialIndex = 0; materialIndex < meshRenderer.materials.Length; materialIndex++) {
+                            Material material = meshRenderer.materials[materialIndex];
+
+                            Debug.Log("Checking material " + material.name + " with " + eyeMaterialName);
+                            if (material.name.Equals(eyeMaterialName + " (Instance)")) {
+                                eyeMaterial = material;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (eyeMaterial != null) {
+                        eyeMaterials[index] = eyeMaterial;
+                    }
+                    else {
+                        Debug.LogError("Failed to find material on object " + eye.name);
+                    }
+                }
+                else {
+                    Debug.LogError("Failed to find mesh renderer on object " + eye.name);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (eyelidAnimators != null && eyelidAnimators.Length > 0) {
+            UpdateEyelids();
+        }
+
+        if (eyes != null && eyes.Length > 0) {
+            UpdateEyes();
+        }
+    }
+
+    private void UpdateEyelids() {
+        timeToNextBlink -= Time.deltaTime;
+
+        if (timeToNextBlink <= 0.0f) {
+            Debug.Log("Blink");
+            for (int index = 0; index < eyelidAnimators.Length; index++) {
+                eyelidAnimators[index].SetTrigger("blink");
+            }
+
+            timeToNextBlink = Random.Range(2.0f, 5.0f);
+        }
+    }
+
+
+    private void UpdateEyes() {
+        for (int index = 0; index < eyes.Length; index++) {
+            Vector2 targetOffset = new Vector2(0,0);
+            if(pointOfInterest != null) {
+                targetOffset = DetermineEyeOffsetForPointOfInterest(index);
+            }
+  
+            UpdateEyeToLookAtTarget(index, targetOffset);
+        }
+    }
+
+    private Vector2 DetermineEyeOffsetForPointOfInterest(int index) {
+        // Calculate the angle between the eye and target point and use that to offset the UV coords
+        GameObject eye = eyes[index];
+        Vector3 facing = eye.transform.forward;
+        Vector3 up = eye.transform.up;
+        Vector3 directionToPointOfInterest =  pointOfInterest.position - eye.transform.position;
+
+        Vector3 directionXZ = new Vector3(directionToPointOfInterest.x, facing.y, directionToPointOfInterest.z);
+        directionXZ.Normalize();
+
+        Vector3 directionY = new Vector3(facing.x, directionToPointOfInterest.y, facing.z);
+        directionY.Normalize();
+
+        float productXZ = Vector3.Dot(directionXZ, facing);
+        float productY = Vector3.Dot(directionY, up);
+
+        //Debug.Log("Facing = " + facing + " Up = " + eye.transform.up);
+        //Debug.Log("XZ = " + productXZ + "("+ directionXZ + ")");
+        //Debug.Log("Y = " + productY + "(" + directionY + ")");
+
+        // Result of the dot product is as follows:
+        // 1 means vectors are parallel in the same direction
+        // 0 means vectors are perpendicular (either side)
+        // > 0 is vectors are opposing i.e. facing away from each other beyond 90 degrees either side, with -1 being directly opposite
+        // We are only interested in the vector in XZ-axes where the resulting dot proiduct is > 0 i.e. in front of the object
+
+        float targetOffsetX = 0.0f;
+        float targetOffsetY = 0.0f;
+
+        if (productXZ >= 0.0f) {
+            // Do a dot product with the tangent vector of the facing direction to determine which side the direction vector is in relation to the facing direction.
+            // This allows us to alter the sign of the final UV offset in the X axis to fit within our -0.5 to 0.5 range.
+            float sign = (Vector3.Dot(directionXZ, eye.transform.right) > 0.0f ? 1.0f : -1.0f);
+            targetOffsetX = ((1.0f - productXZ) * uvOffsetLimit) * sign;
+            // NOTE: The shader uses negative for 'up' so negate the dot product in the y-axis to compensate
+            targetOffsetY = -productY * uvOffsetLimit;
+        }
+
+        return new Vector2(targetOffsetX, targetOffsetY);
+    }
+
+
+    private void UpdateEyeToLookAtTarget(int index, Vector2 targetOffset) {
+        // Linearly interpolate towards the target offsets to avoid instant changes in the eye offsets
+        float offsetX = eyeMaterials[index].GetFloat("_OffsetX");
+        float offsetY = eyeMaterials[index].GetFloat("_OffsetY");
+
+        offsetX = Mathf.Lerp(offsetX, targetOffset.x, eyeInterpolationRate);
+        offsetY = Mathf.Lerp(offsetY, targetOffset.y, eyeInterpolationRate);
+
+        eyeMaterials[index].SetFloat("_OffsetX", offsetX);
+        eyeMaterials[index].SetFloat("_OffsetY", offsetY);
+    }
+}
---
+++ Assets/Shaders/OctopusEye.shader
@@ -0,0 +1,112 @@
+﻿Shader "Custom/OctopusEye"
+{
+	Properties 
+	{
+		_Color("_Color", Color) = (1,1,1,0)
+		_Shininess("Shininess", Range(0.01,1) ) = 1
+		_MainTex("_MainTex", 2D) = "black" {}
+		_OffsetX("_OffsetX", Range(-0.5,0.5) ) = 0
+		_OffsetY("_OffsetY", Range(-0.5,0.5) ) = 0
+	}
+	
+	SubShader 
+	{
+		Tags
+		{
+			"Queue"="Geometry"
+			"IgnoreProjector"="False"
+			"RenderType"="Opaque"
+		}
+
+		
+		Cull Back
+		ZWrite On
+		ZTest LEqual
+		ColorMask RGBA
+		Fog{
+		}
+
+
+		CGPROGRAM
+		#pragma surface surf BlinnPhongEditor  vertex:vert
+		#pragma target 2.0
+
+		float4 _Color;
+		float _Shininess;
+		sampler2D _MainTex;
+		float _OffsetX;
+		float _OffsetY;
+
+		struct EditorSurfaceOutput {
+			half3 Albedo;
+			half3 Normal;
+			half3 Emission;
+			half3 Gloss;
+			half Specular;
+			half Alpha;
+			half4 Custom;
+		};
+			
+		inline half4 LightingBlinnPhongEditor_PrePass (EditorSurfaceOutput s, half4 light) {
+			half3 spec = light.a * s.Gloss;
+			half4 c;
+			c.rgb = (s.Albedo * light.rgb + light.rgb * spec);
+			c.a = s.Alpha;
+			return c;
+		}
+
+		inline half4 LightingBlinnPhongEditor (EditorSurfaceOutput s, half3 lightDir, half3 viewDir, half atten) {
+			half3 h = normalize (lightDir + viewDir);
+				
+			half diff = max (0, dot ( lightDir, s.Normal ));
+				
+			float nh = max (0, dot (s.Normal, h));
+			float spec = pow (nh, s.Specular*128.0);
+				
+			half4 res;
+			res.rgb = _LightColor0.rgb * diff;
+			res.w = spec * Luminance (_LightColor0.rgb);
+			res *= atten * 2.0;
+
+			return LightingBlinnPhongEditor_PrePass( s, res );
+		}
+			
+		struct Input {
+			float2 uv2_MainTex;
+		};
+
+		void vert (inout appdata_full v, out Input o) {
+			o.uv2_MainTex = v.texcoord;
+		}
+			
+
+		void surf (Input IN, inout EditorSurfaceOutput o) {
+			o.Normal = float3(0.0,0.0,1.0);
+			o.Alpha = 1.0;
+			o.Albedo = 0.0;
+			o.Emission = 0.0;
+			o.Gloss = 0.0;
+			o.Specular = 0.0;
+			o.Custom = 0.0;
+				
+			float4 Split0=(IN.uv2_MainTex.xyxy);
+			float4 Add0=_OffsetX.xxxx + float4( Split0.x, Split0.x, Split0.x, Split0.x);
+			float4 Add1=_OffsetY.xxxx + float4( Split0.y, Split0.y, Split0.y, Split0.y);
+			float4 Assemble0=float4(Add0.x, Add1.y, float4( Split0.z, Split0.z, Split0.z, Split0.z).z, float4( Split0.w, Split0.w, Split0.w, Split0.w).w);
+			float4 Tex2D0=tex2D(_MainTex,Assemble0.xy);
+			float4 Multiply0=_Color * Tex2D0;
+			float4 Splat0=_Color.w;
+			float4 Master0_1_NoInput = float4(0,0,1,1);
+			float4 Master0_2_NoInput = float4(0,0,0,0);
+			float4 Master0_5_NoInput = float4(1,1,1,1);
+			float4 Master0_7_NoInput = float4(0,0,0,0);
+			float4 Master0_6_NoInput = float4(1,1,1,1);
+			o.Albedo = Multiply0;
+			o.Specular = _Shininess.xxxx;
+			o.Gloss = Splat0;
+			o.Normal = normalize(o.Normal);
+		}
+		ENDCG
+	}
+	Fallback "Diffuse"
+}
